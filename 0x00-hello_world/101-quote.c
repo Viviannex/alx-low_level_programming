229653dc6c25dbff52306ed52ec15ea178b29661
Viviannex <vivianebassem@gmail.com>
2023-08-10 19:17:24 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - main function
  *
- * Description: print a qoute using write function
- *   ssize_t write(int fd. const void *buf, size_t count);
- *
- *   Return: 1 (error)
+ *   Return: 1
  */
 
 int main(void)
-{       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1. quo, 59);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
